1c0e9fdc42c1d0e8d86cd44684353c7d0a8c5cb1
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-12 02:34:28 +0530

    defined static controller and wrote view

--- web/controllers/uploadControllers.py
+++ web/controllers/uploadControllers.py
@@ -1,3 +1,36 @@
+from re import I
+from flask import (render_template , 
+                    request as req , 
+                    redirect , 
+                    flash , 
+                    session)
 
 def static():
-    pass
+    '''
+    Test Cases
+    1) title root description not be empty
+    2) root should always be *.html / *.htm
+    3) file should always be *.zip
+    4) root_file_name to be present at root of file 
+    '''
+    errs=""
+    title=req.form['title'].strip()
+    root=req.form['root'].strip()
+    description=req.form['description'].strip()
+    file = req.files['file']
+    filename=file.filename
+    parsed_name,ext=filename.rsplit(".",1)
+    
+    if title=="" or root=="" or description=="":
+        errs+="Upload Information not Specified,"
+    if root!="" and not (root.rsplit(".",1) in ['htm','html']):
+        errs+="The Specified root file is not html,"
+    if filename=="" or ext!='zip':
+        errs+=' The Uploading file is not specified or is not zip .'
+    if(errs==""):
+        flash(errs)
+    else:
+        flash("Uploading Zip..")
+    return redirect('/dashboard/new-site')
+    
+    

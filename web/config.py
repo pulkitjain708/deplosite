c7f671349b08fa7746a767217e0c8ad288f53357
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-12 03:21:31 +0530

    changed config , added logic to save file

--- web/config.py
+++ web/config.py
@@ -1,4 +1,6 @@
 import os
+from platform import system
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,3 +12,10 @@ DATABASE='deplo'
 MONGO_URI='mongodb+srv://root:root@deplo.tevs8.mongodb.net/{DATABASE}?retryWrites=true&w=majority'
 
 MAX_CONTENT_LENGTH=15*1000*1000
+
+
+# UPLOAD_PATH=""
+if system()=="Windows":
+    UPLOAD_PATH="C:\\Users\\intern\\project\\uploads"
+elif system()=="Linux":
+    UPLOAD_PATH='~/deplosite/uploads'
--- web/controllers/uploadControllers.py
+++ web/controllers/uploadControllers.py
@@ -4,6 +4,9 @@ from flask import (render_template ,
                     redirect , 
                     flash , 
                     session)
+from config import UPLOAD_PATH
+from werkzeug.utils import secure_filename
+import os
 
 def static():
     '''
@@ -12,6 +15,7 @@ def static():
     2) root should always be *.html / *.htm
     3) file should always be *.zip
     4) root_file_name to be present at root of file 
+    5) only allowed files- media , css , js html
     '''
     errs=""
     title=req.form['title'].strip()
@@ -20,17 +24,19 @@ def static():
     file = req.files['file']
     filename=file.filename
     parsed_name,ext=filename.rsplit(".",1)
-    
+    username=session['username']
     if title=="" or root=="" or description=="":
         errs+="Upload Information not Specified,"
-    if root!="" and not (root.rsplit(".",1) in ['htm','html']):
+    if root!="" and not (root.rsplit(".",1)[1] in ['htm','html']):
         errs+="The Specified root file is not html,"
     if filename=="" or ext!='zip':
         errs+=' The Uploading file is not specified or is not zip .'
     if(errs==""):
-        flash(errs)
-    else:
+        filename = secure_filename(filename)
+        file.save(os.path.join(UPLOAD_PATH, filename))
         flash("Uploading Zip..")
+    else:
+        flash(errs)
     return redirect('/dashboard/new-site')
     
     

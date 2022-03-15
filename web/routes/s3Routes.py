0a5d6778ab0c706d0373b4c9264e06c54be6e6cc
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-16 04:00:16 +0530

    POST TO GET

--- web/routes/s3Routes.py
+++ web/routes/s3Routes.py
@@ -4,6 +4,6 @@ import controllers
 s3RouteBP = Blueprint('upload', __name__)
 
 s3RouteBP.route('/static', methods=['POST'])(controllers.s3Controllers.static)
-s3RouteBP.route('/delete/<string:bucketName>', methods=['POST'])(controllers.s3Controllers.delete)
+s3RouteBP.route('/delete/<string:bucketName>', methods=['GET'])(controllers.s3Controllers.delete)
 
     

55abf781eacd91065908be97cddbbb5885624217
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-20 18:12:53 +0530

    NumberOfObjects,BucketSizeBytes

--- web/common.py
+++ web/common.py
@@ -1,21 +1,45 @@
-from datetime import datetime
-import boto3
+# from datetime import datetime
+# import boto3
 
-cw = boto3.client('cloudwatch')
+#  BucketSizeBytes
+# NumberOfObjects
+# ReplicationLatency
+# 4xxErrors
+# TotalRequestLatency
+# AllRequests
 
-response = cw.get_metric_statistics(Namespace='AWS/S3', Dimensions=[
-    {
-        'Name': 'BucketName',
-        'Value': 'rainblur-78034-deplosite.co'
-    },
-    {
-        'Name': 'StorageType',
-        'Value': 'StandardStorage'
-    }
-],
-    MetricName='BucketSize',
-    StartTime=datetime(2022, 3, 1),
-    EndTime=datetime(2022, 3, 20),
-    Statistics=["SampleCount", "Average", "Sum", "Minimum",'Maximum'],
-    Period=60*60*24)
-print(response)
+# cw = boto3.client('cloudwatch')
+
+# response = cw.get_metric_statistics(Namespace='AWS/S3', Dimensions=[
+#     {
+#         'Name': 'BucketName',
+#         'Value': 'blog-59796-deplosite.co'
+#     },
+#     {
+#         'Name': 'StorageType',
+#         'Value': 'StandardStorage'
+#     }
+# ],
+#     MetricName='BucketSizeBytes',
+#     StartTime=datetime(2022, 3, 1),
+#     EndTime=datetime(2022, 3, 20),
+#     Statistics=["SampleCount", "Average", "Sum", "Minimum",'Maximum'],
+#     Period=60*60*24)
+
+# response = cw.get_metric_statistics(Namespace='AWS/S3', Dimensions=[
+#     {
+#         'Name': 'BucketName',
+#         'Value': 'blog-59796-deplosite.co'
+#     },
+#     {
+#         'Name': 'StorageType',
+#         'Value': 'AllStorageTypes'
+#     }
+# ],
+#     MetricName='NumberOfObjects',
+#     StartTime=datetime(2022, 3, 1),
+#     EndTime=datetime(2022, 3, 20),
+#     Statistics=["SampleCount", "Average", "Sum", "Minimum",'Maximum'],
+#     Period=60*60*24)
+# print(response)
+# 

3a64d15c9c3ed21b3f5b48327b8a48f96c18c91e
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-23 13:29:37 +0530

    s3 log download and deletion

--- web/t1.py
+++ web/t1.py
@@ -1,3 +1,4 @@
+# CODE TO FETCH s3 free metrics
 from datetime import datetime
 import boto3
 
@@ -35,3 +36,21 @@ for bucket in s3.list_buckets()['Buckets']:
             Statistics=["SampleCount", "Average", "Sum", "Minimum", 'Maximum'],
             Period=60*60*24*2)
         print(response['Datapoints'])
+
+
+# CODE TO PARSE LOGS as CSV
+
+import pandas as pd
+
+df = pd.read_csv('r.csv', sep=" ", names=['Bucket Owner', 'Bucket', 'Time', 'Time - Offset', 'Remote IP', 'Requester ARN/Canonical ID',
+                                          'Request ID',
+                                          'Operation', 'Key', 'Request-URI', 'HTTP status', 'Error Code', 'Bytes Sent', 'Object Size',
+                                          'Total Time',
+                                          'Turn-Around Time', 'Referrer', 'User-Agent', 'Version Id', 'Host Id', 'Signature Version',
+                                          'Cipher Suite',
+                                          'Authentication Type', 'Host Header', 'TLS version'],
+                 usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                          13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                 engine='python')
+                 
+df.to_csv('r2.csv')
---
+++ web/t2.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+
+s3 = boto3.client('s3')
+response = s3.list_objects(
+    Bucket='deplosite-logging')
+prevFileName=""
+
+l=[]
+
+for item in response['Contents']:
+    currentFileName=item['Key'].split(":")[0]
+    pathName=os.path.join(os.getcwd(),'logs',currentFileName)
+    if prevFileName!=currentFileName:
+        exists=os.path.exists(pathName)
+        if not exists:
+            os.mkdir(pathName,mode = 0o666)
+    prevFileName=currentFileName
+    l.append(item['Key'])
+    s3.download_file('deplosite-logging',item['Key'],os.path.join(pathName,item['Key'].split(":")[1]))
+
+
+for item in l:
+    s3.delete_object(Bucket='deplosite-logging',Key=item)
+    
+

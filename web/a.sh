286cef8d1fdde414077b6f3677321136a498fdaa
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-24 04:02:37 +0530

    using cron

--- web/a.sh
+++ web/a.sh
@@ -1,1 +1,7 @@
 wget http://127.0.0.1:5000/s3/getLogs
+
+# start cron : sudo service cron start
+# stop cron : sudo service cron stop
+# enable cron : sudo systemctl enable cron
+# write cron : crontab -e
+# 0 0 * * * <name of command> runs every midnight

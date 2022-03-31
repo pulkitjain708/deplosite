4375945c24b3bbf7a3c2663675a776494938629e
pulkit jain <pulkit.jain-intern@verananetworks,com>
2022-03-31 19:38:56 +0530

    integrated google auth

--- web/models/user.py
+++ web/models/user.py
@@ -7,55 +7,66 @@ from pymongo import MongoClient
 def getDb(collection="user"):
     try:
         mongo = MongoClient(config.MONGO_URI)
-        print("Connection Object , ",mongo)
+        print("Connection Object , ", mongo)
         return mongo.deplo[collection] if collection else mongo.deplo
     except Exception as e:
-        print(e," : Error at Connecting DB")
+        print(e, " : Error at Connecting DB")
+
 
 class User():
-    user={}
-    def __init__(self,username="",email="",password=""):
-        self.username=username
-        self.email=email
-        self.password=password
+    user = {}
 
-        self.user={
-            "username":self.username,
-            "email":self.email,
-            "password":self.password
+    def __init__(self, username="", email="", password=""):
+        self.username = username
+        self.email = email
+        self.password = password
+
+        self.user = {
+            "username": self.username,
+            "email": self.email,
+            "password": self.password
         }
+
+    def getId(self):
+        flag = getDb().find_one(
+            {"username": self.username, "email": self.email}, {"_id": 1})
+        if flag:
+            return flag["_id"]
+        else:
+            return flag
 
     def save(self):
         try:
-            flag=self.doesExist()
+            flag = self.doesExist()
             if flag == False:
                 getDb().insert_one(self.user)
                 return True
             else:
-                flag=False
+                flag = False
         except Exception as e:
             print(e)
-            flag=False
+            flag = False
         return flag
 
-    def doesExist(self,login=0):
-        if login==1:
+    def doesExist(self, login=0):
+        if login == 1:
             try:
-                flag=getDb().find_one({"username":self.username,"password":self.password})
-                print('flag value : ',flag,file=sys.stdout)
+                flag = getDb().find_one(
+                    {"username": self.username, "password": self.password})
+                print('flag value : ', flag, file=sys.stdout)
                 return flag
             except Exception as e:
-                print(e,file=sys.stdout)
+                print(e, file=sys.stdout)
                 return False
         else:
             try:
-                username=getDb().find_one({"username":self.username})
-                email=getDb().find_one({"email":self.email})
+                username = getDb().find_one({"username": self.username})
+                email = getDb().find_one({"email": self.email})
                 if (username and email) or (username or email):
-                    flag=True
+                    flag = True
                 else:
-                    flag=False
+                    flag = False
             except Exception as e:
-                flag=True
+                flag = True
                 print(e)
             return flag
